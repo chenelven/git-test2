c33353f31ec1f0062718e23f620467398f068c48
elven <evlen@qq.com>
2022-12-16 20:21:36 +0800

    final commit

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("这msater的第一次提交");
         System.out.println("这是hot-fix的第一次提交");
+        System.out.println("提交了");
     }
 }
