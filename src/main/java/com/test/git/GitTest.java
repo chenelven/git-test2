2e35a03fd4a8473203b6701b681b934d826879e4
elven <evlen@qq.com>
2022-12-16 20:00:27 +0800

    master commit

---
+++ src/main/java/com/test/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.test.git;
+
+/**
+ * @author elven
+ * @create 2022-12-16 19:59
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("这msater的第一次提交");
+    }
+}
