5945cf6e61fa33c59a27a30da5165d92098f1c43
elven <evlen@qq.com>
2022-12-16 20:02:30 +0800

    hot-fix first commit

--- src/main/java/com/test/git/GitTest.java
+++ src/main/java/com/test/git/GitTest.java
@@ -7,5 +7,6 @@ package com.test.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("这msater的第一次提交");
+        System.out.println("这是hot-fix的第一次提交");
     }
 }
